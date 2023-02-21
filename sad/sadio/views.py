b3ee39090e3557824f7a9ce3ea4175a36ccd171b
Parsa Mohammadian <parsa2820@gmail.com>
2023-02-21 16:19:45 +0330

    Fix pipeline

--- sad/sadio/views.py
+++ sad/sadio/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .lib.libhandler import create_library
-
 def index(request):
     return render(request, "sadio/index.html", {})
 
