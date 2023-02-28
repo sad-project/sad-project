358003a520be78b22ea9ab9b1003839e3b2dd4b9
mmdrez4 <mohammadreza.dn80@gmail.com>
2023-02-28 17:59:47 +0330

    create get_user function and complete index function

--- sad/sadio/views.py
+++ sad/sadio/views.py
@@ -4,30 +4,52 @@ from django.http import HttpResponse
 from django.db import models
 
 USERNAME_TEXT_INPUT_KEY = "username-text-input"
-PASSWORD_PASSWORD_iNPUT_KEY = "password-password-input"
+PASSWORD_PASSWORD_INPUT_KEY = "password-password-input"
 LOGIN_SUBMIT_INPUT_KEY = "login-submit-input"
 REGISTER_SUBMIT_INPUT_KEY = "register-submit-input"
 LOGINED_USER_KEY = "logined_user"
+LOGIN_MESSAGE_KEY = ""
+
+
+def get_user(username):
+    users = User.objects.all()
+    for user in users:
+        if user.username == username:
+            return user
+    return None
 
 
 def index(request):
+    context = {}
     if request.method == 'POST':
         username = request.POST.get(USERNAME_TEXT_INPUT_KEY, None)
-        password = request.POST.get(PASSWORD_PASSWORD_iNPUT_KEY, None)
-        if LOGIN_SUBMIT_INPUT_KEY:
+        password = request.POST.get(PASSWORD_PASSWORD_INPUT_KEY, None)
+        print(username)
+        if LOGIN_SUBMIT_INPUT_KEY in request.POST:
             # TODO: Check username and password here
-            # if username == 
             # retrieve user from database and compare
-            request.session[LOGINED_USER_KEY] = username
-            users = User.objects.all()
-            return redirect("libraries")
-        elif REGISTER_SUBMIT_INPUT_KEY:
+            print("qwer")
+            user = get_user(username)
+            print(user)
+            if user:
+                if user.password == password:
+                    request.session[LOGINED_USER_KEY] = username
+                    print("You are logged in!")
+                    return redirect("libraries")
+                else:
+                    print("Wrong inputs for login!")
+                    pass
+
+        elif REGISTER_SUBMIT_INPUT_KEY in request.POST:
             # TODO: Create user here
             # add user to database
-
-            pass
-    return render(request, "sadio/index.html")
-
+            if not get_user(username):
+                record = User(username=username, password=password)
+                record.save()
+                context[LOGIN_MESSAGE_KEY] = "You are registered!"
+            else:
+                context[LOGIN_MESSAGE_KEY] = "Wrong inputs for register!"
+    return render(request, "sadio/index.html", context)
 
 def libraries(request):
     context = {}
