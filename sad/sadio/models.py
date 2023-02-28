a79ceb218df1b02a0579d2d562d274e18a305e18
mmdrez4 <mohammadreza.dn80@gmail.com>
2023-02-28 16:49:15 +0330

    update view.py

--- sad/sadio/models.py
+++ sad/sadio/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
+
 class User(models.Model):
     username = models.CharField(max_length=255, primary_key=True)
     password = models.CharField(max_length=255)
+
 
 class Library(models.Model):
     name = models.CharField(max_length=255, primary_key=True)
     owner = models.ForeignKey("User", on_delete=models.CASCADE)
     bucket = models.CharField(max_length=255)
+
 
 class File(models.Model):
     name = models.CharField(max_length=255)
--- sad/sadio/views.py
+++ sad/sadio/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.db import models
 
 USERNAME_TEXT_INPUT_KEY = "username-text-input"
 PASSWORD_PASSWORD_iNPUT_KEY = "password-password-input"
@@ -7,18 +9,25 @@ LOGIN_SUBMIT_INPUT_KEY = "login-submit-input"
 REGISTER_SUBMIT_INPUT_KEY = "register-submit-input"
 LOGINED_USER_KEY = "logined_user"
 
+
 def index(request):
     if request.method == 'POST':
         username = request.POST.get(USERNAME_TEXT_INPUT_KEY, None)
         password = request.POST.get(PASSWORD_PASSWORD_iNPUT_KEY, None)
         if LOGIN_SUBMIT_INPUT_KEY:
             # TODO: Check username and password here
+            # if username == 
+            # retrieve user from database and compare
             request.session[LOGINED_USER_KEY] = username
+            users = User.objects.all()
             return redirect("libraries")
         elif REGISTER_SUBMIT_INPUT_KEY:
             # TODO: Create user here
+            # add user to database
+
             pass
     return render(request, "sadio/index.html")
+
 
 def libraries(request):
     context = {}
@@ -27,6 +36,7 @@ def libraries(request):
     context["logined_user"] = logined_user
     # TODO: Load libraries
     return render(request, "sadio/libraries.html", context)
+
 
 def library(request):
     return HttpResponse("This is x library")
